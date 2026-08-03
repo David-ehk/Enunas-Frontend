95e3e9b1e0f32464900443227927aa36bbc86b12
David Konan <davidemmanuel.konan@gmail.com>
2026-08-03 16:24:14 +0200

    New Hamburger Icon

--- app/Homepage/components/navbar.tsx
+++ app/Homepage/components/navbar.tsx
@@ -139,9 +139,8 @@ const Navbar = () => {
 
   const HamburgerIconSvg = (props: React.SVGProps<SVGSVGElement>) => (
     <svg width="25" height="25" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-      <path d="M4 3H20" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-      <path d="M4 12H20" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-      <path d="M4 21H20" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M4 9H20" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M4 15H20" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
     </svg>
   )
 
